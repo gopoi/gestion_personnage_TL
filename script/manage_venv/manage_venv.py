a59dc63c9539c80c354175be157eeafd1d3f286d
Simon Bastien-Filiatrault <root@gopoi.net>
2018-04-12 23:57:40 -0400

    WIP rellay tiny bit of werk

--- script/manage_venv/manage_venv.py
+++ script/manage_venv/manage_venv.py
@@ -1,24 +1,31 @@
+#!/usr/bin/env python3
+
+import os
 import venv
 
 
 # Create a venv
-class VenvCreator:
+class VenvCreator(venv.EnvBuilder):
 
     def __init__(self, env_dir: str, scripts_dir: str, bare_scripts_dir: bool = True):
         self.env_dir = env_dir
         self.scripts_dir = scripts_dir
         self.bare_scripts_dir = bare_scripts_dir
 
-        self.venv = venv.EnvBuilder(system_site_packages=False,
+        super().__init__(system_site_packages=False,
                                     clear=False,
                                     symlinks=False,
                                     upgrade=False,
                                     with_pip=True)
-        self.context = self.venv.ensure_directories(self.env_dir)
+        self.context = self.ensure_directories(self.env_dir)
 
-    def create_venv():
-        pass
-# Manage existing venv
-# parse options
+    def create_venv(self):
+        self.create(self.env_dir)
+        if self.bare_scripts_dir:
+            os.symlink(self.env_dir, "common")
+        else:
+            pass  # TODO
+
 if __name__ == '__main__':
     pass
+
