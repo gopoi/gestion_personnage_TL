d6b113cb8f541b4f78afac10cb43d425265c6161
Simon Bastien-Filiatrault <root@gopoi.net>
2018-04-14 20:02:05 -0400

    WIP Advancement on creator class

--- script/manage_venv/manage_venv.py
+++ script/manage_venv/manage_venv.py
@@ -7,25 +7,28 @@ import venv
 # Create a venv
 class VenvCreator(venv.EnvBuilder):
 
-    def __init__(self, env_dir: str, scripts_dir: str, bare_scripts_dir: bool = True):
+    def __init__(self, env_dir: str, scripts_dir: str, bare_scripts_dir: bool = True, *args, **kwargs):
         self.env_dir = env_dir
         self.scripts_dir = scripts_dir
         self.bare_scripts_dir = bare_scripts_dir
 
-        super().__init__(system_site_packages=False,
-                                    clear=False,
-                                    symlinks=False,
-                                    upgrade=False,
-                                    with_pip=True)
+        with_pip = kwargs.pop('with_pip', True)
+        super().__init__(with_pip=with_pip, *args, **kwargs)
         self.context = self.ensure_directories(self.env_dir)
 
+    def install_scripts(self, *args, **kwargs):
+
+        super().install_scripts(*args, **kwargs)
+        
     def create_venv(self):
         self.create(self.env_dir)
-        if self.bare_scripts_dir:
-            os.symlink(self.env_dir, "common")
+        if self.bare_scripts_dir and os.name == 'posix':
+            os.symlink(self.env_dir, 'posix')
         else:
             pass  # TODO
 
-if __name__ == '__main__':
-    pass
 
+if  __name__ == '__main__':
+    v=VenvCreator('tesset', 'posix', bare_scripts_dir=False)
+    v.create_venv()
+    v.install_scripts(v.context, '.')
